3697dbdaaa230f8a41cfe3b29fc1e044f2707536
TH-Reuss <thomasmuoz@gmail.com>
2025-06-29 16:49:44 -0400

    added: cars model

--- cars/models.py
+++ cars/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from core.models import BaseModel
 
-# Create your models here.
+class Car(BaseModel):
+    plate = models.CharField("Placa", max_length=20, unique=True)
+    model = models.CharField("Modelo", max_length=100)
+    brand = models.CharField("Marca", max_length=100)
+    manufacture_date = models.DateField("Fecha de fabricación")
+
+    class Meta:
+        verbose_name = "Car"
+        verbose_name_plural = "Cars"
+
+    def __str__(self):
+        return f"{self.brand} {self.model} ({self.plate})"
