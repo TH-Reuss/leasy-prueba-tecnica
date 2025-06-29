cf98bff359f3613138c498cd85d8188f3aa8c5c2
TH-Reuss <thomasmuoz@gmail.com>
2025-06-29 16:45:57 -0400

    added: clients models

--- clients/models.py
+++ clients/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.utils import timezone
+from core.models import BaseModel
 
-# Create your models here.
+class Client(BaseModel):
+    name = models.CharField("Nombre", max_length=150)
+    last_name = models.CharField("Apellido", max_length=150)
+    dni = models.CharField("DNI", max_length=50, unique=True)
+
+    class Meta:
+        verbose_name = "Client"
+        verbose_name_plural = "Clients"
+
+    def __str__(self):
+        return f"{self.name} ({self.document})"
