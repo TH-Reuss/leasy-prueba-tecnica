b4b6416d91bf1c0b6a216243581498f03f2257f0
TH-Reuss <thomasmuoz@gmail.com>
2025-07-01 20:59:54 -0400

    removed: create invoice url

--- contracts/urls.py
+++ contracts/urls.py
@@ -10,5 +10,4 @@ urlpatterns = [
     path('',  ContractListView.as_view(),  name='list'),
     path('create/',  ContractCreateView.as_view(),  name='create'),
     path('invoices/',  InvoiceListView.as_view(),  name='invoice-list'),
-    path('invoices/create/',  InvoiceCreateView.as_view(),  name='invoice-create'),
 ]
