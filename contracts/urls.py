c16b111535d97cc558ad4a1185fe57479c58bec9
TH-Reuss <thomasmuoz@gmail.com>
2025-07-01 21:00:46 -0400

    removed: import invoice create view

--- contracts/urls.py
+++ contracts/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 from .views import (
-    ContractListView, InvoiceListView,
-    ContractCreateView, InvoiceCreateView
+    ContractListView,
+    ContractCreateView,
+    InvoiceListView,
 )
 
 app_name = 'contracts'
